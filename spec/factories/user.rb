139891805d42ebfb7a4ee7d407834b011df6f9b6
Date-Koki <k.date.0320@gmail.com>
2025-09-06 09:49:22 +0900

    テストコードの記載

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     last_name { '山田' }
     first_name_kana { 'リクタロウ' }
     last_name_kana { 'ヤマダ' }
-    birthday { Faker.date.birthday }
+    birthday { Faker::Date.birthday }
   end
 end
