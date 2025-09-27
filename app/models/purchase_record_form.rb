fbf1b77a4589d61e35dbb91e950fb047c501f9fc
Date-Koki <k.date.0320@gmail.com>
2025-09-27 18:25:43 +0900

    修正

--- app/models/purchase_record_form.rb
+++ app/models/purchase_record_form.rb
@@ -3,14 +3,16 @@ class PurchaseRecordForm
   attr_accessor :token, :postcode, :prefecture_id, :municipality, :street_address, :building_name, :telephone_number, :user_id,
                 :item_id
 
-  validates :postcode, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :street_address, presence: true
-  validates :telephone_number, presence: true, format: { with: /\A(\d{10}|\d{11})\z/, message: 'is invalid' }
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :token, presence: true
+  with_options presence: true do
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :municipality
+    validates :street_address
+    validates :telephone_number, format: { with: /\A(\d{10}|\d{11})\z/, message: 'is invalid' }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
